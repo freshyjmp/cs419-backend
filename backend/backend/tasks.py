0c072a1f6ff1fb9cb657509d18a911e26b13f016
Jon Patterson <jon.m.patterson@gmail.com>
2016-10-23 21:41:38 -0700

    a few small changes to handle some errors. Putting this up on hosting so that other people can get at it.

--- backend/backend/tasks.py
+++ backend/backend/tasks.py
@@ -8,7 +8,7 @@ We would like for these tasks to be performed asynchronously, with the
 
 """
 
-import requests
+import requests, validators
 from celery import Celery, Task
 from celery.contrib.methods import task_method
 from backend.parser import WaltzHTMLParser
@@ -30,12 +30,32 @@ def get_links_on_page(url, request, max_depth, current_depth,
         raise ValueError("Expected to get a response for %s, but didn't! Abort! Abort!" % (url))
     parser = WaltzHTMLParser()
     parser.feed(data=response.text)
+    kw_was_found = False
+    if keyword is not None:
+        if response.txt.find(keyword != -1):
+                kw_was_found = True
+
     for link in list(set(parser.links)):
-        e = Edge(request, url, link, current_depth, False)
+
+        if link.startswith('/'):
+            link = url + link
+
+        if not validators.url(url):
+            print("result of validators for %s: %r "  % ( link, validators.url(link)))
+            # Try to see if it's a relative link or something
+            # simple that would case this validator to fail
+            if not validators.url(url + link):
+                # give up, we can't fix it
+                continue
+            # it worked! move on with this as the link
+            link = url + link
+
+        e = Edge(request, url, link, current_depth, kw_was_found)
         db.session.add(e)
         db.session.commit()
         # Prepare for danger! Make another task for each link! #YOLO#
         if current_depth < max_depth:
+            print("generating task for %s at %d" % (link, current_depth+1))
             get_links_on_page.delay(link, request, max_depth, 
                                         current_depth+1, keyword, searchmode)
     return "impolite words here"
