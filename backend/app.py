cb1abf9c358ef91fc1d4499bfe7ce4731ecf5bee
Jon Patterson <jon.m.patterson@gmail.com>
2016-10-29 13:47:32 -0700

    adding some CORS stuff so frontend can run on another server. going to have team test

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 
@@ -7,16 +8,20 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-    #
+    #Database set up stuff
     from backend.db import db
     from backend.models import Request, Edge
     db.app = app
     db.init_app(app)
     db.create_all()
 
-    #
+    #Using apimanager for some blueprints
     from backend.api import apimanager
     apimanager.init_app(app, flask_sqlalchemy_db=db)
+
+    #Turn on CORS permissively for now
+    CORS(app)
+
     @app.route('/')
     def hello():
         return "Hello dank wizard"
