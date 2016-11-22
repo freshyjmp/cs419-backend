7f00f8807cd40489c9245488327a8972aa12cd41
Jon Patterson <jon.m.patterson@gmail.com>
2016-11-21 20:05:07 -0800

    adding another view to allow chunked updates

--- backend/views.py
+++ backend/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint,jsonify
 from backend.app import create_app
 from backend.db import db
 from backend.models import Edge
-
+from sqlalchemy import and_
 
 results = Blueprint('results_page', __name__)
 @results.route('/api/results/<int:request_id>')
@@ -14,4 +14,12 @@ def show_result(request_id):
         query_result[index] = row_as_dict
     return jsonify(rows=len(query_result),edges=query_result)
 
-
+@results.route('/api/results/<int:request_id>/<int:row_number>')
+def show_chunked_result(request_id, row_number):
+    query_result = Edge.query.filter(and_(Edge.request == request_id,
+      Edge.id > row_number)).limit(1000).all()
+    print("RequestId:%d, starting from Id:%d, getting %d rows" % (request_id, row_number, len(query_result)))
+    for index, row in enumerate(query_result):
+        row_as_dict = row.as_dict()
+        query_result[index] = row_as_dict
+    return jsonify(rows=len(query_result),edges=query_result)
